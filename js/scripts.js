42857ae647d381bbfaa0934e787c9eb84c1bab0b
Yuliia Bogoliub <julia.bogolub@gmail.com>
2025-01-16 18:30:05 +0200

    Made section Testimonial and contact

--- js/scripts.js
+++ js/scripts.js
@@ -10,3 +10,31 @@ function toggleTheme() {
 }
 
 toggleTheme();
+
+
+document.getElementById('copyButton').addEventListener('click', function() {
+    const copyText = document.getElementById('copyText');
+    copyText.select(); // Виділяємо текст
+    copyText.setSelectionRange(0, 99999); // Для мобільних пристроїв
+    navigator.clipboard.writeText(copyText.value) // Копіюємо текст у буфер
+      .then(() => {
+        alert('Текст скопійовано: ' + copyText.value);
+      })
+      .catch(err => {
+        console.error('Помилка копіювання: ', err);
+      });
+  });
+
+  document.getElementById('btnCopy').addEventListener('click', function() {
+    const textInput = document.getElementById('textInput');
+    textInput.select(); // Виділяємо текст
+    textInput.setSelectionRange(0, 99999); // Для мобільних пристроїв
+    navigator.clipboard.writeText(textInput.value) // Копіюємо текст у буфер
+      .then(() => {
+        alert('Текст скопійовано: ' + textInput.value);
+      })
+      .catch(err => {
+        console.error('Помилка копіювання: ', err);
+      });
+  });
+  
